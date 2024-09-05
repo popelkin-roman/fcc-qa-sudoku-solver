bcd6b60198265ad9d29964a20f86d57291ce8ebd
popelkin-roman <mymaythe23@gmail.com>
2024-09-05 22:06:10 +0500

    feat: add check row placement

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -6,7 +6,14 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    let sudokuRows = [];
+    let testRow=''
+    for (let i = 0; i < 9; i++) {
+      let row = puzzleString.slice( 9 * i, 9 * i + 9);
+      sudokuRows.push(row);
+    }
+    testRow=sudokuRows[row].slice(0, column) + sudokuRows[row].slice(column + 1);
+    return !testRow.includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
