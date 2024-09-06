2b35cddad2b23d857e5b3a36207943df0fff1297
popelkin-roman <mymaythe23@gmail.com>
2024-09-06 18:13:26 +0500

    feat: add check column and region placement; refactor row placement

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -6,22 +6,27 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-    let sudokuRows = [];
-    let testRow=''
-    for (let i = 0; i < 9; i++) {
-      let row = puzzleString.slice( 9 * i, 9 * i + 9);
-      sudokuRows.push(row);
-    }
-    testRow=sudokuRows[row].slice(0, column) + sudokuRows[row].slice(column + 1);
+    let testRow = puzzleString.slice(9*row, 9*row + column) + puzzleString.slice(9*row + column + 1, 9*row + 9);
     return !testRow.includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    let testCol=''
+    for (let i = 0; i < 81; i+=9) {
+      if (i / 9 === row) continue;
+      testCol += puzzleString[i + column]
+    }
+    return !testCol.includes(value);
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    let testRegion='';
+    let startRow = Math.floor(row/3)*3
+    let startCol = Math.floor(column/3)*3
+    for (let i = 0; i < 3; i++) {
+      testRegion += puzzleString.slice(9*(startRow+i) + startCol, 9*(startRow+i) + startCol + 3)
+    }
+    return !testRegion.includes(value);
   }
 
   solve(puzzleString) {
