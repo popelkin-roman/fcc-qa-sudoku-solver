0510b164eda1c77ed573cfdbb26cbff40d019b71
popelkin-roman <mymaythe23@gmail.com>
2024-09-07 14:49:05 +0500

    feat: add solve method to SudokuSolver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -30,7 +30,46 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
+    if (!this.validate(puzzleString)) return false;
+    let solutions = [puzzleString];
+    let result = false;
+    let checkPlacement = (puzzleString, row, column, value) => {
+      return this.checkRowPlacement(puzzleString, row, column, value)
+        && this.checkColPlacement(puzzleString, row, column, value)
+        && this.checkRegionPlacement(puzzleString, row, column, value)
+    }
     
+    for (let index=0; index<puzzleString.length; index++) {
+      for (let solLine = 0; solLine < solutions.length; solLine++) {
+        if (solutions[solLine][index] === '.') {
+          let values = getValues(solutions[solLine], index);
+          let newLines = values.map( val => solutions[solLine].slice(0, index) + val + solutions[solLine].slice(index+1))
+          solutions = [...solutions.slice(0, solLine), ...newLines, ...solutions.slice(solLine+1)]
+          if (newLines.length === 0) solLine--;
+        }
+      }
+    }
+
+    function getValues(puzzleString, index) {
+      let result = []
+      for (let i = 1; i <= 9; i++) {
+        if (checkPlacement(puzzleString, getRow(index), getCol(index), i)) {
+          result.push(i)
+        }
+      }
+      return result
+    }
+    function getRow(index) {
+      return Math.floor(index / 9);
+    }
+    function getCol(index) {
+      return index % 9;
+    }
+
+    solutions.forEach(el => {
+      if (!el.includes('.')) result = el;
+    })
+    return result
   }
 }
 
